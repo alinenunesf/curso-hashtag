ef84d4ed0a057fecd0d39ec0de7a3d65da4e633f
Aline Nunes <alinenunes3105@hotmail.com>
2025-02-16 14:00:29 -0300

    ajuste no process env

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,8 +1,11 @@
-import "dotenv/config";
+import dotenv from "dotenv";
+dotenv.config();
 import axios from "axios";
 
-const { NODE_ENV } = process.env;
-const URL = NODE_ENV === 'development' ? "http://localhost:3000/api" : "/api";
+// const { NODE_ENV } = process.env;
+// const URL = NODE_ENV === 'development' ? "http://localhost:3000/api" : "/api";
+
+const URL = "https://curso-hashtag.onrender.com/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
