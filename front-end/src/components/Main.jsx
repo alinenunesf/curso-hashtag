64fe30f054e31f941828f311221e2e3ad7b6425c
Aline Nunes <alinenunes3105@hotmail.com>
2025-02-16 12:44:04 -0300

    projeto finalizado

---
+++ front-end/src/components/Main.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCirclePlay } from "@fortawesome/free-solid-svg-icons/faCirclePlay";
+import ItemList from './ItemList';
+import { artistArray } from '../assets/database/artists';
+import { songsArray } from '../assets/database/songs';
+
+const Main = ({ type }) => {
+    return (
+    <div className="main">
+        {/* Item list de artistas */}
+        {type === "artists" || type === undefined ? (<ItemList 
+        title="Artistas" 
+        items={10} 
+        itemsArray={artistArray} 
+        path="/artists"
+        idPath="/artist"
+        />) : ( <></>
+        )}
+        
+
+        {/* Item list de músicas */}
+        {type === "songs" || type === undefined ? ( <ItemList 
+        title="Músicas" 
+        items={20} 
+        itemsArray={songsArray} 
+        path="/songs"
+        idPath ='/song'
+        />) : ( <></>
+        )}
+        
+    </div>
+    );
+};
+
+export default Main;
